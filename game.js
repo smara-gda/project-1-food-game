fc2ae041b34dcdcba364a975d491abe7100541f0
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2020-11-26 19:39:38 +0100

    added method for dropping foods from above

--- game.js
+++ game.js
@@ -1,17 +1,12 @@
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
-const width = canvas.width;
-const height = canvas.height;
-
 class Game {
   constructor() {
-    this.player = new Player();
-    this.badFoods = [new BadFood(height)];
+    this.player = new Player(cnvWidth / 2, cnvHeight - 50, 50, 50);
+    this.badFoods = [new BadFood(cnvHeight)];
     this.goodFoods = [new GoodFood()];
     this.lastBadFoodTimeStamp = 0;
     this.lastGoodFoodTimeStamp = 0;
     this.setKeyListeners();
-    console.log(this.badFoods);
+    // console.log(this.badFoods);
   }
   // function to make the player move around on the screen
   setKeyListeners() {
@@ -36,11 +31,11 @@ class Game {
   // setting border so player does not move out of canvas
   avoidGoingOutOfCanvas() {
     this.player.playerX = Math.max(
-      Math.min(this.player.playerX, width - this.player.playerWidth),
+      Math.min(this.player.playerX, cnvWidth - this.player.playerWidth),
       0
     );
     this.player.playerY = Math.max(
-      Math.min(this.player.playerY, height - this.player.playerHeight),
+      Math.min(this.player.playerY, cnvHeight - this.player.playerHeight),
       0
     );
   }
@@ -54,10 +49,7 @@ class Game {
     });
   }
 
-  // TODO rainingFoods(foodType){}
-
-  // call runLogic method for every element in the game that has it
-  runLogic() {
+  rainingFoods() {
     const currentTimeStamp = Date.now();
     // make food items appear every 3 and 7 secs
     if (currentTimeStamp > this.lastBadFoodTimeStamp + 2000) {
@@ -68,11 +60,16 @@ class Game {
       this.goodFoods.push(new GoodFood());
       this.lastGoodFoodTimeStamp = currentTimeStamp;
     }
-    // run bad food runLogic methos for each element in its array
+  }
+
+  // call runLogic method for every element in the game that has it
+  runLogic() {
+    this.rainingFoods();
+    // run bad food runLogic method for each element in its array
     for (let badFood of this.badFoods) {
       badFood.runLogic();
     }
-    // run good food runLogic methos for each element in its array
+    // run good food runLogic method for each element in its array
     for (let goodFood of this.goodFoods) {
       goodFood.runLogic();
     }
@@ -94,8 +91,3 @@ class Game {
     this.drawGoodAndBadFoods();
   }
 }
-
-const game = new Game();
-game.loop();
-
-// TODO score
