83c51798971ba0ab4fa447f52c6eeb159516a449
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2020-11-26 19:40:14 +0100

    separated the loop from the game class

---
+++ script.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+const cnvWidth = canvas.width;
+const cnvHeight = canvas.height;
+
+const game = new Game();
+game.loop();
+
+/* TODO: 
+-score, 
+-lose points when touching bad food,
+-gain points when touching good food, 
+-erase foods from the array when touching them or when they are out of the canvas.
+-Maybe have lives, if you eat bad food 3 times, you die, game over? 
+*/
