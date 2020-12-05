34552f2965c162486fccf0228feaabcfc0594104
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2020-12-05 11:06:18 +0100

    adding background music

--- game.js
+++ game.js
@@ -53,6 +53,8 @@ class Game {
   gameOver() {
     gamingScreen.style.display = 'none';
     gameOverSection.style.display = 'block';
+    backgroundMusic.pause();
+    backgroundMusic.currentTime = 0;
   }
   resetGame() {
     this.score = 0;
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const backgroundMusic = new Audio('sounds/Jungle Friends.mp3');
+backgroundMusic.playbackRate = 0.9;
+backgroundMusic.volume = 0.3;
+
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 const cnvWidth = canvas.width;
@@ -16,6 +20,7 @@ const scoreSpan = document.querySelector('.score span');
 
 startElement.addEventListener('click', () => {
   gamingScreen.style.display = 'block';
+  backgroundMusic.play();
   startingSection.style.display = 'none';
   startElement.style.display = 'none';
   scoreElement.style.display = 'block';
@@ -25,6 +30,7 @@ startElement.addEventListener('click', () => {
 replayElement.addEventListener('click', () => {
   gameOverSection.style.display = 'none';
   gamingScreen.style.display = 'block';
+  backgroundMusic.play();
   scoreElement.style.display = 'block';
   scoreSpan.innerHTML = '0';
   game.condition = true;
